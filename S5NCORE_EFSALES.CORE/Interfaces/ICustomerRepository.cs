b40a9d8cbcfe4fb71b5704d8b762153b87014c96
Luis Chang <luis.chang@outlook.com>
2021-10-27 21:44:40 -0500

    Agregar archivos de proyecto.

---
+++ S5NCORE_EFSALES.CORE/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using S5NCORE_EFSALES.CORE.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace S5NCORE_EFSALES.CORE.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<bool> Delete(int id);
+        Task<Customer> GetCustomerById(int id);
+        Task<IEnumerable<Customer>> GetCustomers();
+        Task<bool> Insert(Customer customer);
+        Task<bool> Update(Customer customer);
+    }
+}
---
+++ S5NCORE_EFSALES.INFRASTRUCTURE/Repositories/CustomerRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using S5NCORE_EFSALES.CORE.Entities;
+using S5NCORE_EFSALES.CORE.Interfaces;
+using S5NCORE_EFSALES.INFRASTRUCTURE.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace S5NCORE_EFSALES.INFRASTRUCTURE.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly SalesDBContext _context;
+
+        public CustomerRepository(SalesDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Customer>> GetCustomers()
+        {
+            return await _context.Customer.ToListAsync();
+        }
+        public async Task<Customer> GetCustomerById(int id)
+        {
+            return await _context.Customer.FindAsync(id);
+        }
+
+        public async Task<bool> Insert(Customer customer)
+        {
+            _context.Customer.Add(customer);
+            int countRows = await _context.SaveChangesAsync();
+            return (countRows > 0);
+        }
+
+        public async Task<bool> Update(Customer customer)
+        {
+            var customerNow = await _context.Customer.FindAsync(customer.Id);
+            customerNow.FirstName = customer.FirstName;
+            customerNow.LastName = customer.LastName;
+            customer.City = customer.City;
+            customerNow.Country = customer.Country;
+            customerNow.Phone = customer.Phone;
+
+            int countRows = await _context.SaveChangesAsync();
+            return (countRows > 0);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var customerNow = await _context.Customer.FindAsync(id);
+
+            _context.Customer.Remove(customerNow);
+            int countRows = await _context.SaveChangesAsync();
+            return (countRows > 0);
+        }
+
+
+
+
+    }
+}
